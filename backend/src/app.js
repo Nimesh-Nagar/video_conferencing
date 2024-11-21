3c8f1f2bc5c54247fc0fee010f37bbfd514f2a67
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-11-22 00:46:11 +0530

    create db mode

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import { connectToSocket } from "./controllers/socketManager.js";
+import mongoose from "mongoose";
+
+import cors from "cors";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000 ))
+
+// app.get("/home", (req,res) => {
+//     return res.json( {"hello":"word"} )
+// });
+
+app.use(cors());
+app.use(express.json({limit:"40kb"}))
+app.use(express.urlencoded({ limit : "40kb", extended : true }));
+
+
+const start = async() => {
+    const connectionDb = await mongoose.connect("mongodb+srv://nimesh:nimesh22@cluster0.29f6u.mongodb.net/")
+
+    console.log(`MONGODB connected to DB : ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log(`LISTENING on PORT ${app.get("port")}`);
+    });
+}
+
+start();
+
