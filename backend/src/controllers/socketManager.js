12b649ac81d14baed0e19e3dca5f3c4b5b9cf197
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-11-24 12:38:59 +0530

    socket setup for client connection

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -1,7 +1,83 @@
-import { Server } from "socket.io"
+import { connections } from "mongoose"
+import { Server, Socket } from "socket.io"
+
+let connection = {}
+let messages = {}
+let timeOnline = {}
 
 export const connectToSocket = (server) => {
     const io = new Server(server);
 
-    return io
+    io.on("connection", (socket) => { 
+        console.log("Client Connected");
+
+        socket.on("join-call", (path) => {
+
+        })
+
+        // to establish webRTC P2P connection in app 
+        socket.on("signal", (toId, messages) => {
+            io.to(toId).emit("signal", socket.id, messages)
+        })
+
+
+        socket.on("chat-message", (data, sender) => {
+            const [matchingRoom, found] = Object.entries(connections).reduce( ([room, isFound],[roomKey, roomValue]) => {
+
+                if( !isFound && roomValue.includes(sockets.id)) {
+                    return [roomKey, true];
+                }
+
+                return [roomKey, isfound];
+            }, ['', flase ]);
+
+            if (found === true) {
+                if (messages[matchingRoom] === undefined) {
+                    messages[matchingRoom] = []
+                }
+
+                messages[matchingRoom].push({ 'sender': sender, "data": data, "socket-id-sender": socket.id })
+                console.log("message", matchingRoom, ":", sender, data)
+
+                connections[matchingRoom].forEach((elem) => {
+                    io.to(elem).emit("chat-message", data, sender, socket.id)
+                })
+            }
+
+        })
+
+        socket.on("disconnect", () => {
+            var diffTime = Math.abs(timeOnline[socket.id] - new Date())
+
+            var key
+
+            for (const [k, v] of JSON.parse(JSON.stringify(Object.entries(connections)))) {
+
+                for (let a = 0; a < v.length; ++a) {
+                    if (v[a] === socket.id) {
+                        key = k
+
+                        for (let a = 0; a < connections[key].length; ++a) {
+                            io.to(connections[key][a]).emit('user-left', socket.id)
+                        }
+
+                        var index = connections[key].indexOf(socket.id)
+
+                        connections[key].splice(index, 1)
+
+
+                        if (connections[key].length === 0) {
+                            delete connections[key]
+                        }
+                    }
+                }
+
+            }
+
+        })
+
+    });
+
+    return io;
 }
+
